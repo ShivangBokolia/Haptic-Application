df12fa24b99f408f21289543c8dab7b8ae54bce3
PalaviBhole <palavibhole@gmail.com>
2023-03-21 20:19:58 -0400

    participant10

--- app/src/main/java/com/example/hapticapplication/AAHapticCommon.java
+++ app/src/main/java/com/example/hapticapplication/AAHapticCommon.java
@@ -23,7 +23,7 @@ import java.util.Date;
 import java.util.Locale;
 
 public class AAHapticCommon {
-    static String user="Participant_09";
+    static String user="Participant_10";
 
     static Context context;
     static private Uri paths;
